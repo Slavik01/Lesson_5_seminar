e78dde0198830d92f14475bbf7ad05ec76413c18
Slavik <slavik-skripka@yandex.ru>
2022-07-13 18:13:17 +0300

    version 1

---
+++ Homework_5_seminar/One/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[size]; // выделяем память new int[size] для элементов которые введем с консоли и записываем в массив positivNumbers
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    arrayNumbers[i] = new Random().Next(100, 1000);// c 0  индекста рандомные числа
+}
+Console.WriteLine($"Массив: [{String.Join("; ", arrayNumbers)}]");
+    for (int k = 0; k < arrayNumbers.Length; k++)
+    {
+        if ((arrayNumbers[k] % 2) == 0) // поиск кол-во четного числа 
+        {
+        count++; Console.WriteLine($"Четные числа из массива: {arrayNumbers[k]}"); // счетчик для подсчета кол-во числе и вывод чисел из массива
+        }
+        
+        
+    }
+
+//Console.WriteLine($"Массив: [{String.Join("; ", arrayNumbers)}]");
+//Console.WriteLine("Четные: " + count);
+
---
+++ Homework_5_seminar/Three/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+int[] numbers = {3, 7, 22, 2, 78};
+int max = 0;
+int min = numbers [0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine($"Разница равна: {max - min}");
